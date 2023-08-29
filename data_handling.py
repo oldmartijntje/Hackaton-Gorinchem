279b4bb168d6d0f37ba12fcaa4d2cb19b543c3e0
99062735 <99067235@mydavinci.nl>
2023-08-29 14:06:44 +0200

    Declassified data_handling.py

--- data_handling.py
+++ data_handling.py
@@ -2,32 +2,38 @@ import json
 import discord
 
 
-class DataHandling:
-    def create_server(self, interaction: discord.Interaction):
-        try:
-            server_id = interaction.guild_id
-            with open('data.json', 'r') as server_data:
-                new_server_data = json.loads(server_data.read())['testServerId']
-            with open('data.json', 'w') as servers:
-                json.loads(servers.read())[server_id] = new_server_data
-            return True
-        except Exception as e:
-            print(e)
-            return False
-
-    def user_is_admin(self, interaction: discord.Interaction):
-        user = interaction.user.name
-        user_is_serveradmin = interaction.user.guild_permissions.administrator
+def create_server(interaction: discord.Interaction):
+    try:
         server_id = interaction.guild_id
-        with open('servers.json', 'r') as servers:
-            admins = json.loads(servers.read())[server_id]['admins']
-        user_is_botadmin = user in admins
-        if user_is_serveradmin:
-            return True
-        elif user_is_botadmin:
-            return True
-        else:
-            return False
+        with open('data.json', 'r') as server_data:
+            new_server_data = json.loads(server_data.read())['testServerId']
+        with open('data.json', 'w') as servers:
+            json.loads(servers.read())[server_id] = new_server_data
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def user_is_admin(interaction: discord.Interaction):
+    server_id = interaction.guild_id
+    user_name = interaction.user.name
+    user_is_serveradmin = interaction.user.guild_permissions.administrator
+    with open('servers.json', 'r') as servers:
+        admins = json.loads(servers.read())[server_id]['admins']
+    user_is_botadmin = user_name in admins
+    if user_is_serveradmin:
+        return True
+    elif user_is_botadmin:
+        return True
+    else:
+        return False
 
 
-
+def add_botadmin(interaction: discord.Interaction):
+    server_id = interaction.guild_id
+    user_name = interaction.user.name
+    with open('data.json', 'r') as data:
+        admins = json.loads(data.read())[server_id]['admins']
+    if user_name in admins:
+        return 'Admin already exists'
+    admins.add(user_name)
