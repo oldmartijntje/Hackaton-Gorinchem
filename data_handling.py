9d1e033f85b796885c4ee472dc203aa28eca1041
99062735 <99067235@mydavinci.nl>
2023-08-29 13:50:14 +0200

    Created a function to check whether a user is admin.

--- data_handling.py
+++ data_handling.py
@@ -3,8 +3,9 @@ import discord
 
 
 class DataHandling:
-    def create_server(self, server_id, interaction: discord.Interaction):
+    def create_server(self, interaction: discord.Interaction):
         try:
+            server_id = interaction.guild_id
             with open('data.json', 'r') as server_data:
                 new_server_data = json.loads(server_data.read())['testServerId']
             with open('data.json', 'w') as servers:
@@ -12,6 +13,20 @@ class DataHandling:
             return True
         except Exception as e:
             print(e)
+            return False
+
+    def user_is_admin(self, interaction: discord.Interaction):
+        user = interaction.user.name
+        user_is_serveradmin = interaction.user.guild_permissions.administrator
+        server_id = interaction.guild_id
+        with open('servers.json', 'r') as servers:
+            admins = json.loads(servers.read())[server_id]['admins']
+        user_is_botadmin = user in admins
+        if user_is_serveradmin:
+            return True
+        elif user_is_botadmin:
+            return True
+        else:
             return False
 
 
