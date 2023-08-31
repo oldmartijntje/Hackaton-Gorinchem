f3071a2859b6f09c1692ad6b165ec02f12227537
oldmartijntje <oldmartijntje@gmail.com>
2023-08-31 10:58:27 +0200

    fixes for data handling

--- admin.py
+++ admin.py
@@ -1,9 +1,10 @@
+import uuid
 import discord
 import data_handling 
 
 async def test(interaction: discord.Interaction, vote_name): 
     if vote_name == '':
-        vote_name = "aaaaaa"
+        vote_name = str(uuid.uuid4())
     if data_handling.user_is_admin(interaction):
-        print("oui oui")
+        print(data_handling.addPollToData(interaction, vote_name))
     await interaction.response.send_message(vote_name)
